cc76dee55019ce015dca5d58754defca4755af96
luca467 <luca467@gmail.com>
2016-12-06 09:51:05 +0100

    Create progexam.c

---
+++ progexam.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"mylib.h"
+
+int main(){
+  int b=0,c;
+  system("clear");
+  do{
+    printf("1) Crea percorso\n");
+    printf("2) Muovi Oberon\n");
+    printf("3) Termina gioco\n");
+    scanf("%d",c);
+    switch (c){
+      case 1{
+        crea_percorso();
+        break;
+      }
+      case 2{
+        muovi_oberon();
+        break;
+      }
+      case 3{
+        termina_gioco();
+      }
+      default{
+      system("clear");
+      }
+    }
+  }while(b==0);
+  return 0;
+}
